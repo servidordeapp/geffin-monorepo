477bc69f812b1ac44b1679010213cbf5a2aee6a1
Bruno Oliveira <bds.commus@gmail.com>
2026-05-23 16:16:25 -0300

    feat: Add AdminUserSeeder for initial admin user setup in database seeding

---
+++ laravel/database/seeders/AdminUserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->createOne([
+            'name' => 'Admin',
+            'email' => 'admin@geffin.com.br',
+        ]);
+    }
+}
