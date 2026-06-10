ea248ab40515c98462f2da55e3054aa8a4655f04
Bruno Oliveira <bds.commus@gmail.com>
2026-06-10 18:54:16 -0300

    Add TenancyServiceProvider to the application service providers

--- app/bootstrap/providers.php
+++ app/bootstrap/providers.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
+use App\Providers\TenancyServiceProvider;
 
 return [
     AppServiceProvider::class,
+    TenancyServiceProvider::class,
 ];
